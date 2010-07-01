4d12d5192988ecc88d3521a6e5c66a3ec8489dd2
Erik Andrejko <erik@shopittome.com>
2010-06-30 20:11:19 -0700

    added multi-term for terminals

--- andrejko.el
+++ andrejko.el
@@ -533,5 +533,22 @@
   (set-face-attribute 'default (selected-frame) :height 
                       (+ (face-attribute 'default :height) (* (if (> n 0) 1 -1) 10))))
 
-(global-set-key (kbd "M-_")      '(lambda nil (interactive) (djcb-zoom 1)))
-(global-set-key (kbd "M-+")      '(lambda nil (interactive) (djcb-zoom -1)))
+(global-set-key (kbd "M-_")      '(lambda nil (interactive) (djcb-zoom -1)))
+(global-set-key (kbd "M-+")      '(lambda nil (interactive) (djcb-zoom 1)))
+
+
+;; Multi-term for terminals
+ (require 'multi-term)
+(autoload 'multi-term "multi-term" nil t)
+(autoload 'multi-term-next "multi-term" nil t)
+
+(setq multi-term-program "/bin/bash")   ;; use bash
+;; (setq multi-term-program "/bin/zsh") ;; or use zsh...
+
+;; only needed if you use autopair
+(add-hook 'term-mode-hook
+  #'(lambda () (setq autopair-dont-activate t)))
+
+
+(global-set-key (kbd "C-c t") 'multi-term-next)
+(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
