5204785514af2ff953827ec7253086fc06dc9087
Erik Andrejko <erik@shopittome.com>
2010-07-04 16:58:45 -0700

    added customization to support clojure

--- custom.el
+++ custom.el
@@ -8,5 +8,14 @@
  '(cc-other-file-alist (quote (("\\.cc\\'" (".hh" ".h")) ("\\.hh\\'" (".cc" ".C")) ("\\.m\\'" (".h")) ("\\.c\\'" (".h")) ("\\.h\\'" (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m")) ("\\.C\\'" (".H" ".hh" ".h")) ("\\.H\\'" (".C" ".CC")) ("\\.CC\\'" (".HH" ".H" ".hh" ".h")) ("\\.HH\\'" (".CC")) ("\\.c\\+\\+\\'" (".h++" ".hh" ".h")) ("\\.h\\+\\+\\'" (".c++")) ("\\.cpp\\'" (".hpp" ".hh" ".h")) ("\\.hpp\\'" (".cpp")) ("\\.cxx\\'" (".hxx" ".hh" ".h")) ("\\.hxx\\'" (".cxx")))))
  '(default-frame-alist (quote ((tool-bar-lines . 0) (fringe) (right-fringe) (left-fringe . 1) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 0) (scroll-bar-background . "grey75") (background-color . "Grey15") (background-mode . dark) (border-color . "Grey") (cursor-color . "Grey") (foreground-color . "Grey") (mouse-color . "Grey"))))
  '(egg-buffer-hide-sub-blocks-on-start nil)
+ '(inferior-lisp-program "java -cp /Users/andrejko/projects/community/clojure/clojure.jar clojure.main")
  '(javascript-indent-level 2)
  '(transient-mark-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ruby-complexity-complexity-high ((t (:foreground "#ff3333"))))
+ '(ruby-complexity-complexity-low ((t (:foreground "#888"))))
+ '(ruby-complexity-complexity-normal ((t (:foreground "#f08888")))))
